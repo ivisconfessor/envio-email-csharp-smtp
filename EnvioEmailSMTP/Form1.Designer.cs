0237ad3751a1a9fb6befffefd25ab4826c031050
Ivís Confessor <ivisconfessor@gmail.com>
2021-10-25 14:48:57 -0300

    Artigo Tech - YouTube | Envio de E-mail com C# via SMTP

---
+++ EnvioEmailSMTP/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace EnvioEmailSMTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmailDestinatario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmailAssunto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmailMensagem = new System.Windows.Forms.TextBox();
+            this.btnEnviarEmail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o e-mail destinatário";
+            // 
+            // txtEmailDestinatario
+            // 
+            this.txtEmailDestinatario.Location = new System.Drawing.Point(18, 43);
+            this.txtEmailDestinatario.Name = "txtEmailDestinatario";
+            this.txtEmailDestinatario.Size = new System.Drawing.Size(431, 20);
+            this.txtEmailDestinatario.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Assunto";
+            // 
+            // txtEmailAssunto
+            // 
+            this.txtEmailAssunto.Location = new System.Drawing.Point(18, 90);
+            this.txtEmailAssunto.Name = "txtEmailAssunto";
+            this.txtEmailAssunto.Size = new System.Drawing.Size(431, 20);
+            this.txtEmailAssunto.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mensagem";
+            // 
+            // txtEmailMensagem
+            // 
+            this.txtEmailMensagem.Location = new System.Drawing.Point(18, 142);
+            this.txtEmailMensagem.Multiline = true;
+            this.txtEmailMensagem.Name = "txtEmailMensagem";
+            this.txtEmailMensagem.Size = new System.Drawing.Size(428, 229);
+            this.txtEmailMensagem.TabIndex = 5;
+            // 
+            // btnEnviarEmail
+            // 
+            this.btnEnviarEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarEmail.Location = new System.Drawing.Point(134, 400);
+            this.btnEnviarEmail.Name = "btnEnviarEmail";
+            this.btnEnviarEmail.Size = new System.Drawing.Size(211, 23);
+            this.btnEnviarEmail.TabIndex = 6;
+            this.btnEnviarEmail.Text = "Enviar E-mail";
+            this.btnEnviarEmail.UseVisualStyleBackColor = true;
+            this.btnEnviarEmail.Click += new System.EventHandler(this.btnEnviarEmail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.btnEnviarEmail);
+            this.Controls.Add(this.txtEmailMensagem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmailAssunto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmailDestinatario);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ARTIGO TECH - Novo E-mail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmailDestinatario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmailAssunto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmailMensagem;
+        private System.Windows.Forms.Button btnEnviarEmail;
+    }
+}
+
---
+++ EnvioEmailSMTP/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace EnvioEmailSMTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviarEmail_Click(object sender, EventArgs e)
+        {
+            // DEDO NERVOSO
+            btnEnviarEmail.Enabled = false;
+
+            // E-MAIL DESTINATÁRIO
+            string emailDestinatario = txtEmailDestinatario.Text;
+
+            // ASSUNTO
+            string emailAssunto = txtEmailAssunto.Text;
+
+            // MENSAGEM
+            string emailMensagem = txtEmailMensagem.Text;
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress("{e-mail do remetente}"); // DE
+            mail.To.Add(emailDestinatario); // PARA
+            mail.Subject = emailAssunto; // ASSUNTO
+            mail.Body = emailMensagem; // MENSAGEM
+
+            using (var smtp = new SmtpClient("smtp.gmail.com", 587))
+            {
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential("{e-mail do remetente}", "{senha do e-mail do remetente}");
+
+                try
+                {
+                    smtp.Send(mail);
+
+                    MessageBox.Show("E-mail Enviado!");
+
+                    txtEmailDestinatario.Text = string.Empty;
+                    txtEmailAssunto.Text = string.Empty;
+                    txtEmailMensagem.Text = string.Empty;
+                    btnEnviarEmail.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Falha ao Enviar E-mail!");
+
+                    btnEnviarEmail.Enabled = true;
+                }
+            }
+        }
+    }
+}
